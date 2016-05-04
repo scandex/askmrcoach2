2036712e1fedf4472c460ccb8c9ba334aabd72e4
Sebastian Camilo Arguelles Niño <sebastian_arguelles@hotmail.com>
2016-05-04 14:37:20 +0000

    fix

---
+++ start.bat
@@ -0,0 +1,10 @@
+nohup java -jar MONGO_URI RIOT_KEY BR &
+nohup java -jar MONGO_URI RIOT_KEY EUNE &
+nohup java -jar MONGO_URI RIOT_KEY EUW &
+nohup java -jar MONGO_URI RIOT_KEY KR &
+nohup java -jar MONGO_URI RIOT_KEY LAN &
+nohup java -jar MONGO_URI RIOT_KEY LAS &
+nohup java -jar MONGO_URI RIOT_KEY NA &
+nohup java -jar MONGO_URI RIOT_KEY OCE &
+nohup java -jar MONGO_URI RIOT_KEY RU &
+nohup java -jar MONGO_URI RIOT_KEY TR &
